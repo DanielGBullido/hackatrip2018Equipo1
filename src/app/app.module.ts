8d18054bb52de8fd19aa15aa9006b6514f1bc6d8
danielgbullido <daniel.gbullido@gmail.com>
2018-01-20 14:42:46 +0100

    all npmp modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LayoutModule } from 'angular-admin-lte';    //Loading layout module
+import { BoxModule } from 'angular-admin-lte';
+
 
 import { AppComponent } from './app.component';
 
