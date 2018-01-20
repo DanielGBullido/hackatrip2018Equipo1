ee1309833e7dfcc22e5e3d57568fb47f972e8928
danielgbullido <daniel.gbullido@gmail.com>
2018-01-20 15:05:23 +0100

    layout loaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+import { Router, NavigationStart } from '@angular/router';
+
+
+import { adminLteConf } from '../../config/admin-lte';
+import { LayoutModule } from 'angular-admin-lte';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpHeaders, HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+
+/*****ROUTES******/
+import { APP_ROUTING } from './app.routes';
 
 import { LayoutModule } from 'angular-admin-lte';    //Loading layout module
 import { BoxModule } from 'angular-admin-lte';
+import { adminLteConf } from '../../config/admin-lte';
 
 
 import { AppComponent } from './app.component';
@@ -13,7 +19,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LayoutModule.forRoot(adminLteConf),
+    FormsModule,
+    HttpClientModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
