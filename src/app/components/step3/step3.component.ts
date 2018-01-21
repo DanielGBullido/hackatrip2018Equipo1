75cebe618a8cf24ad295e290277bbaa2e24afecc
Portatil/Javi <javierg.andres@gmail.com>
2018-01-21 12:17:32 +0100

    numero de transaciones

--- src/app/components/step3/step3.component.ts
+++ src/app/components/step3/step3.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, Route } from '@angular/router';
 import { BbvaService } from '../../services/bbva/bbva.service';
+import { HcService } from '../../services/hc/hc.service';
 
 @Component({
   selector: 'app-step3',
@@ -11,7 +12,7 @@ export class Step3Component implements OnInit {
   ahorroInput = 0;
   ahorro: number;
   display = false;
-  constructor(private router: Router,  protected bbvaService: BbvaService) {
+  constructor(private router: Router,  protected bbvaService: BbvaService, protected hcService: HcService) {
     router.events.subscribe(
       event => {
         const urlStep3: string = event['url'];
@@ -20,7 +21,6 @@ export class Step3Component implements OnInit {
           localStorage.setItem('token', token);
           // this.router.navigate(['/step3']);
           console.log(token);
-          
         }
         console.log(event['url']);
       }
@@ -31,10 +31,19 @@ export class Step3Component implements OnInit {
 // console.log('route', this.route);
 console.log('router', this.router);
 this.getInfoBbva(localStorage.getItem('token'));
+this.getDestination(localStorage.getItem('ciudad'));
   }
 
   getInfoBbva(token: string) {
     const fullinfo = this.bbvaService.getFullInfo(token);
+    console.log('infobbva: ', fullinfo);
+  }
+  getDestination(destination: string) {
+    let destinatios ='';
+     this.hcService.getDestination('Porto').subscribe(data => {
+      destinatios = data;
+    });
+    console.log('infoDestino: ', destinatios);
   }
 
   calcular() {
--- src/app/services/bbva/bbva.service.ts
+++ src/app/services/bbva/bbva.service.ts
@@ -14,7 +14,7 @@ export class BbvaService {
     this.http.get(`https://connect.bbva.com/token/authorize?
     client_id=app.bbva.equipo1
     &response_type=code&
-    redirect_uri=${YOURREDIRECTURI}`, {
+    redirect_uri=http://localhost:4200/step3`, {
         headers: new HttpHeaders()
           .set('Authorization', `l7J0la@@f5zVLiTIOlFQVzNjlupcLaySq18yn9J7Ta7Wz8uoZkdN9WnnApQ9tcsO`)
           .set('Content-Type', 'application/json')
@@ -24,14 +24,35 @@ export class BbvaService {
       });
   }
   getFullInfo(token: string) {
- this.http.get('https://apis.bbva.com/customers-sbx/v1/me-full', {
-      headers: new HttpHeaders()
-        .set('Authorization', `jwt ${token}`)
-        .set('Content-Type', 'application/json')
-    }).subscribe(data => {
-      // data is now an instance of type ItemsResponse, so you can do this:
-      return this.results = data;
-    });
+    const body = '';
+    this.http.post(`https://connect.bbva.com/token?grant_type=authorization_code&code=${localStorage.getItem('token')}&redirect_uri=http://localhost:4200/step3`, body, {
+        headers: new HttpHeaders().set('Authorization', `Basic YXBwLmJidmEuZXF1aXBvMTpsN0owbGFAQGY1elZMaVRJT2xGUVZ6TmpsdXBjTGF5U3ExOHluOUo3VGE3V3o4dW9aa2ROOVdubkFwUTl0Y3NP`).set('Content-Type', 'application/json'),
+      }).subscribe((data: any) => {
+        // data is now an instance of type ItemsResponse, so you can do this:
+        this.results = data.access_token;
+        this.http.get('https://apis.bbva.com/cards-sbx/v2/me/cards', {
+          headers: new HttpHeaders()
+            .set('Authorization', `jwt ${this.results}`)
+            .set('Content-Type', 'application/json')
+        }).subscribe((dataa: any) => {
+          // data is now an instance of type ItemsResponse, so you can do this:
+  
+          dataa.data.cards.forEach(element => {
+            this.http.get(`https://apis.bbva.com/cards-sbx/v2/me/cards/${element.id}/transactions`, {
+              headers: new HttpHeaders()
+                .set('Authorization', `jwt ${this.results}`)
+                .set('Content-Type', 'application/json')
+            }).subscribe((datas: any) => {
+              // data is now an instance of type ItemsResponse, so you can do this:
+              if (datas.data.cardTransactions.length) {
+                localStorage.setItem('numeroTrans', datas.data.cardTransactions.length);
+              }
+            });
+          });
+
+        });
+      });
+
   }
 
   postBBVA(YOURCODE: string, YOURREDIRECTURI: string, YOURAPPCREDENTIALS: any, body: any) {
